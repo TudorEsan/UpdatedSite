02d36952d422697c207f638b66d4616e7b62e6e6
Tudor Esan <tudor.esan@icloud.com>
2021-12-07 17:11:33 +0200

    Next + TS setup

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import React from 'react'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Hi!</title>
+      </Head>
+      <p>Hello World</p>
+    </>
+  )
+}
+
+export default Home
